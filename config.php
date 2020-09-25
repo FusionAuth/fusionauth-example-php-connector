880498d78f8e68d301ef585305142ba0c6da35b9
Dan Moore <github@mooreds.com>
2020-09-25 16:16:47 -0600

    Adding in connector endpoint.

--- config.php
+++ config.php
@@ -4,6 +4,7 @@
 $client_id = '83767d78-2927-43fa-baef-b556d7c91c9a';
 $client_secret = 'a_pOJZu8D3Fy7a8fVxlPkVa92HPlnUt8d8JfJmtJL5U';
 $tenant_id = '30663132-6464-6665-3032-326466613934';
+$authorization_header_value = 'supersecretauthheader';
 
 // update if you aren't running the app or FusionAuth in the default location 
 $redirect_uri = 'http://localhost:8000/oauth-callback.php';
---
+++ fusionauthconnector.php
@@ -0,0 +1,56 @@
+<?php
+require __DIR__. '/config.php';
+require __DIR__. '/common.php';
+require __DIR__ . '/vendor/autoload.php';
+
+use Ramsey\Uuid\Uuid;
+
+$input = file_get_contents('php://input');
+
+// check auth header
+$authorization_value = $headers['Authorization'];
+if ($authorization_value !== $authorization_header_value) {
+  error_log("Invalid authorization header value found: ".$authorization_value);
+  return;
+}
+
+$inputobj = json_decode($input);
+if (!$inputobj) {
+  error_log("Invalid JSON");
+  return;
+}
+
+// for debugging the data passed to us
+// error_log(var_export($input, TRUE));
+
+if (!auth($inputobj->loginId, $inputobj->password)) {
+  http_response_code(404);
+  return;
+}
+
+// build the user object
+// in the real world, we'd pull this from the database.
+$obj = [];
+$user = [];
+
+$user['id'] = Uuid::uuid4();
+$user['email'] = $inputobj->loginId;
+$user['active'] = true;
+$user['verified'] = true;
+$user['tenantId'] = $tenant_id;
+
+$data = [];
+$data['favoriteColor'] = 'blue';
+$user['data'] = $data;
+
+$registrations = [];
+$registration = [];
+$registration['applicationId'] = $client_id;
+array_push($registrations, $registration);
+
+$user['registrations'] = $registrations;
+
+$obj['user'] = $user;
+
+echo json_encode($obj);
+?>
