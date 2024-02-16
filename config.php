89e0c845be94b55cc26eb793b9bb7e845750bcca
Dan Moore <dan@fusionauth.io>
2024-02-15 19:35:33 -0700

    updated config to use standard values

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
 
-// definitely update
-$client_id = '1892611a-6b99-4457-913f-f39f7c614eb2';
-$client_secret = '4X5_1OVkZi4ottPOrtdzwbe9lbuXRf2Vo9bZJenMkl0';
-$tenant_id = '30663132-6464-6665-3032-326466613934';
+// update if you aren't running the app or FusionAuth in the default location or you aren't using the kickstart
+$client_id = 'E9FDB985-9173-4E01-9D73-AC2D60D1DC8E';
+$client_secret = 'super-secret-secret-that-should-be-regenerated-for-production';
+$tenant_id = 'd7d09513-a3f5-401c-9685-34ab6c552453';
 $authorization_header_value = 'supersecretauthheader';
 
-// update if you aren't running the app or FusionAuth in the default location 
 $redirect_uri = 'http://localhost:8000/oauth-callback.php';
 $fa_url = 'http://localhost:9011';
